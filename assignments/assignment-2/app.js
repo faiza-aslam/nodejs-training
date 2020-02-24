7efe8124bed6171dc2636318376bfe368ce06c76
Faiza Aslam <faiza.aslam@venturedive.com>
2020-02-24 21:14:57 +0500

    axios/fetch common methods

--- assignments/assignment-2/app.js
+++ assignments/assignment-2/app.js
@@ -1,2 +1,22 @@
-const movies = require('./movies');
-const movie = require('./movie');
+const getCall = require('./movies');
+const call = require('./movie');
+
+//using axios
+getCall('https://swapi.co/api/films/')
+.then((res)=> {
+    console.log(res.data.results.length);
+    printTitles(res.data.results);
+});
+
+//using fetch
+call('https://swapi.co/api/films/?search=The%20Phantom%20Menace&format=json')
+.then((res)=> {
+    console.log(res.results.length);
+    printTitles(res.results);
+});
+
+function printTitles(results) {
+    results.forEach(r => {
+        console.log(r.title);
+    });
+}
--- assignments/assignment-2/movie.js
+++ assignments/assignment-2/movie.js
@@ -1,17 +1,8 @@
 const fetch = require('node-fetch');
-let resp = {};
 
-fetch('https://swapi.co/api/films/?search=The%20Phantom%20Menace&format=json')
-.then(response => response.json())
-.then(res => {
-    console.log("\n\nGetting film with Title: The Phantom Menace");    
-    //console.log(res);
-    resp = res.results;
-    resp.forEach(m => {
-        console.log(m.title);
-    });
-    //console.log(resp);
-})
-.catch(err => console.log("Error occurred: "+err));
+const call = (url) => {
+    return fetch(url)
+    .then(response => response.json());
+};
 
-module.exports = resp;
+module.exports = call;
